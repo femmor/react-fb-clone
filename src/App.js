fbec52349cb1d9b4dc7f28ffef53d2a17d70f5b3
Emmanuel Egomson <fegomson@gmail.com>
2020-08-31 18:28:03 -0500

    add header and the header icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header'
+
+// Components
 
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to React</h1>
+      {/* Header */}
+      <Header/>
+
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import "./Header.css"
+
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import {Avatar, IconButton} from '@material-ui/core'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" alt=""/>
+                <div className="header__input">
+                    <SearchIcon/>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SubscriptionsIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <StorefrontIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
+            </div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Femmy Egomson</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon/>
+                </IconButton>
+                <IconButton>
+                    <ForumIcon/>
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon/>
+                </IconButton>
+                <IconButton>
+                    <KeyboardArrowDownIcon/>
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Header
