e5b4b2986f5ad287e04c9444ff296d80be2821f1
Emmanuel Egomson <fegomson@gmail.com>
2020-08-31 09:12:08 -0500

    add firebase config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Welcome to React</h1>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOkMSET4FBYd5lWPZrDDk03DcIMGm6suU",
+    authDomain: "facebook-clone-98e62.firebaseapp.com",
+    databaseURL: "https://facebook-clone-98e62.firebaseio.com",
+    projectId: "facebook-clone-98e62",
+    storageBucket: "facebook-clone-98e62.appspot.com",
+    messagingSenderId: "631759830474",
+    appId: "1:631759830474:web:48920fbd6f07a1e16fe333",
+    measurementId: "G-G0S59DHBBL"
+  };
